533d1aac79982ff70b6a66277bd3407778cb76a1
Stalker1290 <Stalker1290@mail.ru>
2018-06-20 19:17:00 +0300

    Add support for multiply devices on SSP bus. Driver compatible with old projects

--- Includes/SSP.hpp
+++ Includes/SSP.hpp
@@ -23,20 +23,25 @@ class SSP_t
 {
 public:
 	static SSP_t* get_SSP(uint8_t SSP_num);
+	void Initialize(uint32_t bitrate);
+	void Initialize(uint32_t bitrate, IODescription_t* cselGpioArrPtr);
 	void Initialize(uint32_t bitrate, SSEL_type_t SSEL_type);
+	void Initialize(uint32_t bitrate, IODescription_t* cselGpioArrPtr, uint32_t cselNum);
 	void Enable(void);
 	void Disable(void);
 	uint16_t TxRx_frame(uint16_t data);
+	uint16_t TxRx_frame(uint16_t data, uint32_t deviceNum);
 	uint32_t TxRx_frame(void* tx_data, void* rx_data, uint32_t length);
+	uint32_t TxRx_frame(void* tx_data, void* rx_data, uint32_t length, uint32_t deviceNum);
 	void Deinitialize(void);
 private:
 	static SSP_t* SSP_ptrs[Num_of_SSP];
 	SSP_t(uint8_t SSP_num);
 	uint8_t SSP_num;
-	SSEL_type_t SSEL_type;
 	static LPC_SSP_T* SSP_ptr[];
 	static const CHIP_RGU_RST_T ResetNumber[];
-
+	IODescription_t* cselGpioArrPtr;
+	uint32_t cselNum;
 };
 
 
