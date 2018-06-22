a6444f28910ac117e74fc6f30a1e84d3cd40212f
Stalker1290 <Stalker1290@mail.ru>
2018-06-22 17:25:12 +0300

    First working version of FRAM CY15B104Q driver

--- Sources/FRAM_CY15B104Q.cpp
+++ Sources/FRAM_CY15B104Q.cpp
@@ -7,8 +7,10 @@
 
 #include "FRAM_CY15B104Q.hpp"
 #include "stdlib.h"
-
-
+#include "string.h"
+#ifdef FRAM_CY15B104Q_console
+#include "stdio.h"
+#endif
 
 FRAMcy15b104q_t::FRAMcy15b104q_t(SSP_t* SSPPtr, uint32_t SSPDeviceNum){
 	this->SSPDeviceNum = SSPDeviceNum;
@@ -16,6 +18,9 @@ FRAMcy15b104q_t::FRAMcy15b104q_t(SSP_t* SSPPtr, uint32_t SSPDeviceNum){
 }
 
 bool FRAMcy15b104q_t::Initialize(){
+
+	uint32_t manufactureId0 = 0;
+	uint32_t manufactureId1 = 0;
 
 	uint8_t* deviceIdPtr = (uint8_t*)NULL;
 	if(this->SSPPtr == (SSP_t*)NULL){
@@ -29,12 +34,31 @@ bool FRAMcy15b104q_t::Initialize(){
 
 	this->dataExchange(RDID_OPCODE,0,NULL,deviceIdPtr,DEVICE_ID_SIZE);
 
-	#ifdef FRAM_CY15B104Q_console
-		printf("Device ID: %X%X%X%X%X%X%X%X%X", deviceIdPtr[0],deviceIdPtr[1],deviceIdPtr[2],
-				deviceIdPtr[3],deviceIdPtr[4],deviceIdPtr[5],deviceIdPtr[6],deviceIdPtr[7],
-				deviceIdPtr[8]);
-	#endif
+	manufactureId1 = (deviceIdPtr[0]<<16) | (deviceIdPtr[1]<<8) | deviceIdPtr[2];
+	manufactureId0 = (deviceIdPtr[3]<<24) | (deviceIdPtr[4]<<16) | (deviceIdPtr[5]<<8) | deviceIdPtr[6];
 
+	if((manufactureId0 != MANUFACTURER_ID_0) || (manufactureId1 != MANUFACTURER_ID_1)){
+		#ifdef FRAM_CY15B104Q_console
+		printf("Error initialize FRAM! Unknown manufacture ID! \n\r");
+		#endif
+		free_s(deviceIdPtr);
+		return false;
+	}
+	#ifdef FRAM_CY15B104Q_console
+	else{
+		printf("\n\rSSP Device num: %i\n\r"
+				"Found Cypress FRAM!\n\r"
+				"Family: %i\n\r"
+				"Density: %i\n\r"
+				"Sub code: %i\n\r"
+				"Revision: %i\n\r\n\r",
+				this->SSPDeviceNum,
+				(deviceIdPtr[7]&FAMILY_bm)>>5,
+				(deviceIdPtr[7]&DENSITY_bm),
+				(deviceIdPtr[8]&SUB_CODE_bm)>>6,
+				(deviceIdPtr[8]&REVISION_bm)>>3);
+	}
+	#endif
 	free_s(deviceIdPtr);
 
 	return true;
@@ -44,7 +68,7 @@ bool FRAMcy15b104q_t::dataExchange(uint8_t opCode, uint32_t address, uint8_t* tx
 
 	uint8_t* txArrPtr = (uint8_t*)NULL;
 	uint8_t* rxArrPtr = (uint8_t*)NULL;
-
+	
 	switch(opCode){
 	case WREN_OPCODE:
 	case WRDI_OPCODE:
@@ -55,10 +79,10 @@ bool FRAMcy15b104q_t::dataExchange(uint8_t opCode, uint32_t address, uint8_t* tx
 	case RDSR_OPCODE:
 	case WRSR_OPCODE:
 	{
-		volatile uint8_t txArr[2] = {opCode,0};
-		volatile uint8_t rxArr[2] = {0,0};
+		uint8_t txArr[2] = {opCode,0};
+		uint8_t rxArr[2] = {0,0};
 
-		if (txData_ptr != (uint8_t*) NULL){
+		if (txDataPtr != (uint8_t*) NULL){
 			txArr[1] = *txDataPtr;
 		}
 
@@ -101,12 +125,12 @@ bool FRAMcy15b104q_t::dataExchange(uint8_t opCode, uint32_t address, uint8_t* tx
 		}
 
 		if (txDataPtr != (uint8_t*) NULL) {
-			memcpy(&txArrPtr[dataShift], txDataPtr, length);
+			std::memcpy(&txArrPtr[dataShift], txDataPtr, length);
 		}
 
 		if (rxDataPtr != (uint8_t*) NULL) {
 			rxArrPtr = (uint8_t*) malloc_s(finalLen);
-			if ((rxArrPtr == (uint8_t*) NULL)) {
+			if (rxArrPtr == (uint8_t*) NULL) {
 				free_s(txArrPtr);
 				return false;
 			}
@@ -115,7 +139,7 @@ bool FRAMcy15b104q_t::dataExchange(uint8_t opCode, uint32_t address, uint8_t* tx
 		this->SSPPtr->TxRx_frame(txArrPtr, rxArrPtr, finalLen,this->SSPDeviceNum);
 
 		if (rxDataPtr != (uint8_t*) NULL) {
-			memcpy(rxDataPtr, &rxArrPtr[dataShift], length);
+			std::memcpy(rxDataPtr, &rxArrPtr[dataShift], length);
 		}
 
 		free_s(txArrPtr);
@@ -133,8 +157,13 @@ bool FRAMcy15b104q_t::dataExchange(uint8_t opCode, uint32_t address, uint8_t* tx
 }
 
 void FRAMcy15b104q_t::memcpy(void* destination, void* source, uint32_t size){
-
-
+	if((uint32_t)destination < FRAM_SIZE){
+		this->dataExchange(WREN_OPCODE,0,NULL,NULL,0);
+		this->dataExchange(WRITE_OPCODE,(uint32_t)destination,(uint8_t*)source,NULL,size);
+	}
+	else if((uint32_t)source < FRAM_SIZE){
+		this->dataExchange(READ_OPCODE,(uint32_t)source,NULL,(uint8_t*)destination,size);
+	}
 }
 
 
