b0dec6f4c94616f49d3363f68fbda864dd7791e1
Stalker1290 <Stalker1290@mail.ru>
2019-06-06 14:17:49 +0300

    Add "add main procedure" with parameter

--- src/JbControllerM4.cpp
+++ src/JbControllerM4.cpp
@@ -39,14 +39,17 @@ using namespace jbkernel;
 const BoardGpio_t JbController::boardGpios_[] = JBCONTROLLER_BOARD_GPIOS;
 bool JbController::isInitialized = false;
 IVoidCallback* JbController::mainProcedures_[JBCONTROLLER_NUM_MAIN_PROCEDURES];
+void* JbController::mainProceduresParameters_[JBCONTROLLER_NUM_MAIN_PROCEDURES];
 
 
 
 void JbController::initialize(void)
 {
 	if(!isInitialized) {
-		for(uint32_t i = 0; i < JBCONTROLLER_NUM_MAIN_PROCEDURES; i++)
+		for(uint32_t i = 0; i < JBCONTROLLER_NUM_MAIN_PROCEDURES; i++){
 			mainProcedures_[i] = NULL;
+			mainProceduresParameters_[i] = NULL;
+		}
 		SystemCoreClockUpdate();
 		NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_3);
 		__enable_irq();
@@ -258,9 +261,57 @@ void JbController::doMain(void)
 {
 	for(uint32_t i = 0; i < JBCONTROLLER_NUM_MAIN_PROCEDURES; i++) {
 		if(mainProcedures_[i])
-			mainProcedures_[i]->voidCallback(NULL, NULL);
+			mainProcedures_[i]->voidCallback(NULL,
+					mainProceduresParameters_[i]);
 		else
 			break;
+	}
+}
+
+
+
+void JbController::addMainProcedure(IVoidCallback* callback, void* parameter)
+{
+	for(uint32_t i = 0; i < JBCONTROLLER_NUM_MAIN_PROCEDURES; i++) {
+		if((mainProcedures_[i] == callback) &&
+				mainProceduresParameters_[i] == parameter){
+			break;
+		}
+		if(mainProcedures_[i] == NULL) {
+			mainProcedures_[i] = callback;
+			mainProceduresParameters_[i] = parameter;
+			break;
+		}
+	}
+}
+
+
+
+void JbController::deleteMainProcedure(IVoidCallback* callback, void* parameter)
+{
+	uint32_t index = 0;
+	for(uint32_t i = 0; i < JBCONTROLLER_NUM_MAIN_PROCEDURES; i++) {
+		if((mainProcedures_[i] == callback) &&
+				mainProceduresParameters_[i] == parameter){
+			break;
+		}
+		else
+			index++;
+	}
+	if(index == (JBCONTROLLER_NUM_MAIN_PROCEDURES-1)) {
+		if((mainProcedures_[index] == callback) &&
+				mainProceduresParameters_[index] == parameter){
+			mainProcedures_[index] = NULL;
+			mainProceduresParameters_[index] = NULL;
+		}
+	}
+	else {
+		for(uint32_t i = index; i < (JBCONTROLLER_NUM_MAIN_PROCEDURES - 1); i++) {
+			mainProcedures_[i] = mainProcedures_[i+1];
+			mainProceduresParameters_[i] = mainProceduresParameters_[i+1];
+			if(mainProcedures_[i+1] == NULL)
+				break;
+		}
 	}
 }
 
@@ -268,38 +319,14 @@ void JbController::doMain(void)
 
 void JbController::addMainProcedure(IVoidCallback* callback)
 {
-	for(uint32_t i = 0; i < JBCONTROLLER_NUM_MAIN_PROCEDURES; i++) {
-		if(mainProcedures_[i] == callback)
-			break;
-		if(mainProcedures_[i] == NULL) {
-			mainProcedures_[i] = callback;
-			break;
-		}
-	}
+	addMainProcedure(callback, NULL);
 }
 
 
 
 void JbController::deleteMainProcedure(IVoidCallback* callback)
 {
-	uint32_t index = 0;
-	for(uint32_t i = 0; i < JBCONTROLLER_NUM_MAIN_PROCEDURES; i++) {
-		if(mainProcedures_[i] == callback)
-			break;
-		else
-			index++;
-	}
-	if(index == (JBCONTROLLER_NUM_MAIN_PROCEDURES-1)) {
-		if(mainProcedures_[index] == callback)
-			mainProcedures_[index] = NULL;
-	}
-	else {
-		for(uint32_t i = index; i < (JBCONTROLLER_NUM_MAIN_PROCEDURES - 1); i++) {
-			mainProcedures_[i] = mainProcedures_[i+1];
-			if(mainProcedures_[i+1] == NULL)
-				break;
-		}
-	}
+	deleteMainProcedure(callback, NULL);
 }
 
 
